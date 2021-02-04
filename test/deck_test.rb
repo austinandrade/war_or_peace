44b56501a97363725797b7bc9f938d1f10ce13b5
Austin <austinmandrade@gmail.com>
2021-02-04 15:46:18 -0500

    Create test 2 & test deck class

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
 
+
 class DeckTest < Minitest::Test
   def test_it_exists
     card1 = Card.new(:diamond, 'Queen', 12)
@@ -11,5 +12,14 @@ class DeckTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     assert_instance_of Deck, deck
+  end
+
+  def card_test
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_equal [card1, card2, card3], deck.cards
   end
 end
