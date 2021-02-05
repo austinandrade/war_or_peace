accd91e768f3363f09189713cc6271421c8ea6b1
Austin <>
2021-02-04 20:23:12 -0500

    Create first 4 tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,10 +2,11 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
-
+# require 'pry'
 
 class DeckTest < Minitest::Test
   def test_it_exists
+    # skip
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -14,12 +15,46 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
-  def card_test
+  def test_cards_are_in_deck
+    # skip
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    # binding.pry
+    assert_equal [card1, card2, card3], deck.cards
+  end
+
+  def test_rank_of_card_at
+    # skip
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     assert_equal [card1, card2, card3], deck.cards
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 3, deck.rank_of_card_at(1)
+    assert_equal 14, deck.rank_of_card_at(2)
   end
+
+def test_high_ranking_cards
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+  assert_equal [card1, card3], deck.high_ranking_cards
+  end
+
+
+
+
+
+
+
+
+
+
 end
