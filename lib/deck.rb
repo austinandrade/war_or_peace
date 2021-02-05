8ca792205b975f1d3420335ff5428c7f5dab2c24
Austin <>
2021-02-04 20:22:28 -0500

    Add two methods & tested

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,25 @@
+# require 'pry'
+
 class Deck
   attr_reader :cards
-  def initialize (card)
-    @cards = card
+
+  def initialize(cards)
+    @cards = cards
   end
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+    # binding.pry
+  end
+
+  def high_ranking_cards
+    high_bucket = []
+    @cards.rank.each {|x| >= 11}
+    # if @cards[0].rank >= 11
+    #   asdf
+    # elsif @cards[1].rank >= 11
+    #   asdf
+    # elsif @cards[2].rank >= 11
+    #   asdf
 
 end
